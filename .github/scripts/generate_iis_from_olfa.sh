3eabb02e3e4c4df0034a29302ba44e2d09144f27
Ajam <AjamX101@gmail.com>
2023-09-13 15:31:39 +0545

    Update generate_iis_from_olfa.sh

--- .github/scripts/generate_iis_from_olfa.sh
+++ .github/scripts/generate_iis_from_olfa.sh
@@ -33,6 +33,19 @@
 #       : .txt & .html files
 #From "./onelistforall.txt"
 grep -i '.accdb$\|.accdr$\|.asar$\|.asax$\|.ash$\|.ashx$\|.asmx$\|.asp$\|.aspx$\|.axd$\|.bat$\|.bin$\|.cnf$\|.conf$\|.config$\|.csv$\|.dat$\|.dll$\|.doc$\|.docm$\|.docx$\|.eml$\|.eps$\|.evt$\|.evtx$\|.html$\|.ini$\|.iso$\|.jar$\|.log$\|.mas$\|.msi$\|.potm$\|.ppsm$\|.ppsx$\|.pptx$\|.rar$\|.rtf$\|.sldm$\|.sldx$\|.sys$\|.tmp$\|.txt$\|.wadl$\|.wbk$\|.wks$\|.wsdl$\|.xlsm$\|.xltm$\|.xml$\|.xps$\|.zip$' "./onelistforall.txt" | grep -iv '.txt$\|.html$' | sed 's/^http\(\|s\):\/\///g' | grep -vE '^\.|[+%~]|^\*' | sed 's/^\///' | sed -E 's/[[:alpha:]]*[[:digit:]][[:alpha:]]*//g; /^[[:space:]]*$/d' | sort -u -o "/tmp/iis_large.txt"
+#Filter
+ #Remove /
+ sed 's/^\/\+//' -i "/tmp/iis_large.txt"
+ #Remove anything starting with Dots
+ sed 's/^\.\+//' -i "/tmp/iis_large.txt"
+ #Repeat
+ sed 's/^\/\+//' -i "/tmp/iis_large.txt" ; sed 's/^\.\+//' -i "/tmp/iis_large.txt"
+ sed 's/^\/\+//' -i "/tmp/iis_large.txt" ; sed 's/^\.\+//' -i "/tmp/iis_large.txt"
+ sed 's/^\/\+//' -i "/tmp/iis_large.txt" ; sed 's/^\.\+//' -i "/tmp/iis_large.txt"
+ #Remove lines with spaces In Between
+ sed -E '/\s+/d' -i "/tmp/iis_large.txt"
+ #Remove Special Chars
+ sed -E '/^[:"'\''#$&)(*\-]/d' -i "/tmp/iis_large.txt"
 #Fetch from origin
  cat "$GITHUB_WORKSPACE/main/Misc/IIS/iis_mid.txt" | anew -q "/tmp/iis_large.txt"
 #Sort and add to exsisting
